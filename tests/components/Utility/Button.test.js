beec383b66f3ccf15051b8984e7e60afedc566a5
Andrej Tomić <tomicandrej71@gmail.com>
2018-06-03 12:20:03 +0200

    button tests rewritten

--- tests/components/Utility/Button.test.js
+++ tests/components/Utility/Button.test.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import { Button } from '../../../src/components/Utility/Button';
-import { shallow } from 'enzyme'
+import { Button } from '../../../src/components/Button';
+import { shallow, mount } from 'enzyme'
 import renderer from 'react-test-renderer';
 
 describe('Button utility component', () => {
-    const onClickFunc = jest.fn();
-    const props = {
-        type: "something",
-        text: "Andrej's Button",
-        onClick: onClickFunc
-    }
+    let myButton, props, onClickFunc;
+    beforeEach(() => {
+        onClickFunc = jest.fn();
+        props = {
+            classes: "something",
+            text: "Andrej's Button",
+            clickHandler: onClickFunc
+        }
+    })
     it('matches the expected snapshot', () => {
-        const myButtonn = renderer.create(<Button {...props} />).toJSON(); 
-        expect(myButtonn).toMatchSnapshot();
+        const myButton = renderer.create(<Button {...props} />).toJSON();
+        expect(myButton).toMatchSnapshot();
     });
-    
+
     it('correctly calls the onClick function', () => {
-        const myButton = shallow( <Button {...props} />
+        const myButton = shallow(<Button {...props} />
         ).find('button');
         myButton.simulate("click");
         expect(onClickFunc.mock.calls.length).toEqual(1);
+    })
+
+    it('does not call anything when disabled', () => {
+        const myButton = mount(<Button {...props} disabled />).find('button');
+        myButton.simulate("click")
+        expect(onClickFunc.mock.calls.length).toEqual(0)
     })
 })
